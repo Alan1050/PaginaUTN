0e1853618a6792622d1efb7aa6e059b076eaf906
Alan Medina <101961756+Alan1050@users.noreply.github.com>
2026-02-04 00:53:29 -0700

    Ajuste del redireccionamiento

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // Importante para rutas relativas en Ionos
+  // base: './',  // Importante para rutas relativas en Ionos
 })
